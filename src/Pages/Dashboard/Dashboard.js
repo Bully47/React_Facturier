ed052e6b327b2d997d503ee26e49fca7ce5a1511
Vivien <vivien@MacBook-Pro-de-Vivien.local>
2023-01-31 15:25:35 +0100

    Dashboard Page Update

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -2,12 +2,7 @@ import React, { useEffect, useState } from "react"
 import { useAuthState } from "react-firebase-hooks/auth"
 import { useNavigate } from "react-router-dom"
 import { auth, db, logout } from "../../Config/Firebase"
-import {
-	query,
-	collection,
-	getDocs,
-	where
-} from "firebase/firestore"
+import { query, collection, getDocs, where } from "firebase/firestore"
 
 import "./Dashboard.css"
 
@@ -17,8 +12,9 @@ function Dashboard() {
 	const navigate = useNavigate()
 	const fetchUserName = async () => {
 		try {
-			const q = query(collection(db, "users"), where("uid", "==", user?.uid))
-			const doc = await getDocs(q)
+			const doc = await getDocs(
+				query(collection(db, "users"), where("uid", "==", user?.uid))
+			)
 			const data = doc.docs[0].data()
 			setName(data.name)
 		} catch (err) {
@@ -40,6 +36,9 @@ function Dashboard() {
 				<button className="btn btn_bleu" onClick={logout}>
 					Déconnexion
 				</button>
+				<div className="dashboard_recettes_container">
+					{/* Recettes */}
+				</div>
 			</div>
 		</div>
 	)
